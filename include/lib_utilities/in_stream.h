b3f02a86045da5c3871276a3f0112ce7770f38a1
LukasInnerhofer <lukas.ihofer@gmail.com>
2021-08-14 12:16:19 +0200

    Holding a shared_ptr instead of inheriting from basic_istream
    This allows us to construct InStream from any child class of basic_istream

--- include/lib_utilities/in_stream.h
+++ include/lib_utilities/in_stream.h
@@ -1,23 +1,51 @@
 #pragma once
 
 #include <istream>
+#include <memory>
 #include <vector>
 
 namespace LibUtilities
 {
     template <typename T>
-    class InStream : public std::basic_istream<T>
+    class InStream
     {
     public:
+        InStream(std::shared_ptr<std::basic_istream<T>> stream) :
+            m_stream{stream}
+        {
+            if (!m_stream)
+            {
+                throw std::invalid_argument{
+                    "LibUtilities::InStream Error. Cannot construct InStream from invalid basic_istream"};
+            }
+        }
+
         std::vector<T> readAll()
         {
-            seekg(std::ios::end);
-            pos_type const end{tellg()};
+            m_stream->seekg(0, m_stream->end);
+            int64_t const end{m_stream->tellg()};
+            m_stream->seekg(0, m_stream->beg);
             
-            std::vector<T> data{end, 0, std::allocator<T>{}};
-
-            return data;
+            if (end < 0)
+            {
+                std::vector<T> data;
+                T element;
+                while (m_stream->read(&element, 1))
+                {
+                    data.push_back(element);
+                }
+                return data;
+            }
+            else
+            {
+                std::vector<T> data{static_cast<size_t>(end), 0, std::allocator<T>{}};
+                m_stream->read(&*data.begin(), data.size());
+                return data;
+            }
         }
+
+    private:
+        std::shared_ptr<std::basic_istream<T>> m_stream;
     };
 
 }
